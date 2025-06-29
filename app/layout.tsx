f2c9f9399f339cbc52783308e0d32571b8d49150
0xFuckTheFeds <nicholas.a.wenzel@gmail.com>
2025-06-29 12:06:20 -0700

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
